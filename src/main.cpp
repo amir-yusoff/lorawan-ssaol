5df167c3e7441ac3a1fb6884aa27714fdbf3384a
Amir Yusoff <amir.myusoff@gmail.com>
2020-12-02 17:07:27 +0800

    updates to sensor function

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@
 // ************************************************************************************************
 //                                         mode select
 // ************************************************************************************************
-//#define AFFAN_MODE
-#define MB_MODE
-//#define MA_MODE
-//#define MAB_MODE
+#define AFFAN_MODE
+// #define MB_MODE
+// #define MA_MODE
+// #define MAB_MODE
 // ************************************************************************************************
 // ************************************************************************************************
-//#define MY_DEBUG
-//#define LED_STATUS_INDICATOR
+// #define MY_DEBUG
+// #define LED_STATUS_INDICATOR
 
 // sensor and LED pinout
-const int BATTERY_SENSE_PIN = A3;
-const int SOLAR_SENSE_PIN = A4;
+const int BATTERY_SENSE_PIN = 17; // A3
+const int SOLAR_SENSE_PIN = 18;   // A4
 const int LED_PIN = 9;
 #ifdef LED_STATUS_INDICATOR
 const int LED_STATUS_PIN = A0;
@@ -27,12 +27,12 @@ const int LED_PIN2 = A0;
 #endif
 
 // variable declaration
-const float VOLTAGE_REFERENCE = 5;
+const float VOLTAGE_REFERENCE = 5.00;
 bool ledState = LOW;
 unsigned long previousMillis = 0;
 #ifdef AFFAN_MODE
 const float solarThreshold = 1.2;
-const int nightIntensity = 15;
+const int nightIntensity = 9;
 #endif
 #ifdef MB_MODE
 float solarThreshold = 1.6;
@@ -56,7 +56,7 @@ const int dayIntensity = 240;
 const int nightIntensity = 180;
 #endif
 
-// battery-and-solar-monitoring -> mb-node1
+// battery-and-solar-monitoring -> affan-node1
 static const PROGMEM u1_t NWKSKEY[16] = {0x25, 0x8E, 0x23, 0xB5, 0x7D, 0xF1, 0x6F, 0x0E, 0x6D, 0x48, 0x66, 0x79, 0x26, 0xA1, 0x89, 0xE3};
 static const u1_t PROGMEM APPSKEY[16] = {0x72, 0xFB, 0x11, 0xDD, 0xD7, 0x40, 0x02, 0xB4, 0xA3, 0x22, 0xA5, 0xB7, 0x1D, 0xC9, 0x5B, 0x49};
 static const u4_t DEVADDR = 0x26041DBB;
@@ -67,7 +67,7 @@ void os_getDevKey(u1_t *buf) {}
 
 static osjob_t sendjob;
 
-const unsigned TX_INTERVAL = 120;
+const unsigned TX_INTERVAL = 900;
 
 // ************************************************************************************************
 //                                         pin mapping
@@ -348,8 +348,9 @@ void do_send(osjob_t *j)
   else
   {
     // Prepare upstream data transmission at the next possible time.
-    uint16_t b = readBat() * 100;
-    uint16_t s = readSol() * 100;
+    uint16_t b = readBat() * 100.00;
+    uint16_t s = readSol() * 100.00;
+
     byte buffer[4];
     buffer[0] = b >> 8;
     buffer[1] = b;
@@ -357,6 +358,21 @@ void do_send(osjob_t *j)
     buffer[3] = s;
     LMIC_setTxData2(1, buffer, sizeof(buffer), 0);
     printVoltage();
+
+    // Serial.print("byte for readbat() ");
+    // Serial.println(b);
+    // Serial.print("byte for readsol() ");
+    // Serial.println(s);
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //   Serial.print("Payload ");
+    //   Serial.print(i);
+    //   Serial.print(": ");
+    //   Serial.println(buffer[i]);
+    //   //Serial.print(buffer[i]);
+    // }
+    // Serial.println(F(""));
     Serial.println(F("Packet queued"));
   }
   // Next TX is scheduled after TX_COMPLETE event.
@@ -364,15 +380,29 @@ void do_send(osjob_t *j)
 
 float readBat()
 {
-  int battSensorValue = analogRead(BATTERY_SENSE_PIN);
-  float batteryV = battSensorValue * (VOLTAGE_REFERENCE / 1023.0) * 2;
+  float sensorReading = 0.00;
+  float temp = 0.00;
+
+  for (int i = 0; i < 1000; i++)
+  {
+    temp = analogRead(BATTERY_SENSE_PIN);
+    sensorReading += temp;
+  }
+  float batteryV = (sensorReading / 1000.00) * (VOLTAGE_REFERENCE / 1023.00) * 2.00;
   return batteryV;
 }
 
 float readSol()
 {
-  int solSensorValue = analogRead(SOLAR_SENSE_PIN);
-  float solarV = solSensorValue * (VOLTAGE_REFERENCE / 1023.0) * 2;
+  float sensorReading2 = 0.00;
+  float temp2 = 0.00;
+
+  for (int i = 0; i < 1000; i++)
+  {
+    temp2 = analogRead(SOLAR_SENSE_PIN);
+    sensorReading2 += temp2;
+  }
+  float solarV = (sensorReading2 / 1000.00) * (VOLTAGE_REFERENCE / 1023.00) * 2.00;
   return solarV;
 }
 
@@ -381,9 +411,10 @@ void printVoltage()
   int battSensorValue = analogRead(BATTERY_SENSE_PIN);
   int solSensorValue = analogRead(SOLAR_SENSE_PIN);
 
-  float batteryV = battSensorValue * (VOLTAGE_REFERENCE / 1023.0) * 2;
-  float solarV = solSensorValue * (VOLTAGE_REFERENCE / 1023.0) * 2;
+  float batteryV = battSensorValue * (VOLTAGE_REFERENCE / 1023.00) * 2.00;
+  float solarV = solSensorValue * (VOLTAGE_REFERENCE / 1023.00) * 2.00;
 
+  Serial.println("");
   Serial.println("****************************************************");
   Serial.print("Battery sensor reading: ");
   Serial.print(battSensorValue);
@@ -398,4 +429,5 @@ void printVoltage()
   Serial.print(solarV);
   Serial.println(" V");
   Serial.println("****************************************************");
+  Serial.println("");
 }
